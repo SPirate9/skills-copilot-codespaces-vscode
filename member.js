fb37ca0907d378bc99a9a2d3a242a1ff34cad551
SPirate9 <118197355+SPirate9@users.noreply.github.com>
2023-12-23 12:55:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = {
+        name: "Jhon",
+        age: 22,
+        skills: ["C++", "Java", "Python", "Javascript"],
+        address: {
+            city: "New York",
+            country: "USA",
+        },
+    };
+}
